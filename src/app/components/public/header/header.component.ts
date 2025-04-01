0145577f60e879d68a0fab88706a5f72cb078bb0
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-04-01 14:41:44 +0200

    create navbar

--- src/app/components/public/header/header.component.ts
+++ src/app/components/public/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { HeroComponent } from "../hero/hero.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    imports: [NavBarComponent, HeroComponent],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
--- src/app/components/public/public-layout/public-layout.component.ts
+++ src/app/components/public/public-layout/public-layout.component.ts
@@ -3,11 +3,10 @@ import { HeaderComponent } from "../header/header.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-public-layout',
-  standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
-  templateUrl: './public-layout.component.html',
-  styleUrl: './public-layout.component.css'
+    selector: 'app-public-layout',
+    imports: [HeaderComponent, RouterOutlet],
+    templateUrl: './public-layout.component.html',
+    styleUrl: './public-layout.component.css'
 })
 export class PublicLayoutComponent {
 
