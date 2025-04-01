99c4e11285e99f7b4f2637e144dd661943bbb59b
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-04-01 15:23:18 +0200

    update hero

--- src/app/components/public/public-layout/public-layout.component.ts
+++ src/app/components/public/public-layout/public-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+
 
 @Component({
     selector: 'app-public-layout',
+    standalone: true,
     imports: [HeaderComponent, RouterOutlet],
     templateUrl: './public-layout.component.html',
     styleUrl: './public-layout.component.css'
