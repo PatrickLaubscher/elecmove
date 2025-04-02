80a16a2593a93e033e7a59685707a40021d6f464
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-04-02 09:38:29 +0200

    create footer

--- src/app/components/public/homepage/homepage.component.ts
+++ src/app/components/public/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InteractiveMapComponent } from "../interactive-map/interactive-map.component";
+import { CustomerReviewsComponent } from "../customer-reviews/customer-reviews.component";
 
 @Component({
     selector: 'app-homepage',
     standalone: true,
-    imports: [],
+    imports: [InteractiveMapComponent, CustomerReviewsComponent],
     templateUrl: './homepage.component.html',
     styleUrl: './homepage.component.css'
 })
--- src/app/components/public/public-layout/public-layout.component.ts
+++ src/app/components/public/public-layout/public-layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from "../footer/footer.component";
 
 
 @Component({
     selector: 'app-public-layout',
     standalone: true,
-    imports: [HeaderComponent, RouterOutlet],
+    imports: [HeaderComponent, RouterOutlet, FooterComponent],
     templateUrl: './public-layout.component.html',
     styleUrl: './public-layout.component.css'
 })
