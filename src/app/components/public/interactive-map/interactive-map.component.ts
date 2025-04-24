c9363467f33fdc9ff6d825da2d924d84700f608d
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-04-24 11:39:37 +0200

    install mapliter

--- src/app/components/public/interactive-map/interactive-map.component.ts
+++ src/app/components/public/interactive-map/interactive-map.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Map, MapStyle, config } from '@maptiler/sdk';
+import '@maptiler/sdk/dist/maptiler-sdk.css';
 
 @Component({
   selector: 'app-interactive-map',
@@ -6,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './interactive-map.component.html',
   styleUrl: './interactive-map.component.css'
 })
-export class InteractiveMapComponent {
+export class InteractiveMapComponent implements OnInit, AfterViewInit, OnDestroy {
 
+  map: Map | undefined;
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  ngOnInit(): void {
+    config.apiKey = 'GC5T8jKrwWEDcC6F741K';
+  }
+
+  ngAfterViewInit() {
+    const initialState = { lng: 4.850000, lat: 45.750000, zoom: 14 };
+  
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: MapStyle.STREETS,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+  }
+  
+  ngOnDestroy() {
+    this.map?.remove();
+  }
 }
