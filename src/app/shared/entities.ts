4320b0d63fda99ce42d51184120d0c9ed2453618
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-05-29 10:14:02 +0200

    create authentification service

--- src/app/shared/entities.ts
+++ src/app/shared/entities.ts
@@ -1,4 +1,26 @@
 
+
+
+/* Authentication */
+
+export interface AllowedUrl {
+    url: string;
+    methods: string[];
+}
+
+export interface Credentials {
+    username: string;
+    password: string;
+}
+
+export interface Token {
+    token: string;
+    refresh_token: string;
+}
+
+
+
+
 export interface Station {
     lat: number,
     lng: number,
