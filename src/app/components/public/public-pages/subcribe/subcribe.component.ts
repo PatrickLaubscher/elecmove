43af5679a93d89659eb860ea32f6223afd91ff2e
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-05-30 15:07:51 +0200

    create SubscribeComponent

---
+++ src/app/components/public/public-pages/subcribe/subcribe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subcribe',
+  imports: [],
+  templateUrl: './subcribe.component.html',
+  styleUrl: './subcribe.component.css'
+})
+export class SubcribeComponent {
+
+}
